d792a5bcd9604772d10683d0900c2c56485d9e7d
Daniel Krofchick <danielkrofchick@Daniel.local>
2016-07-19 14:25:03 -0400

    Showing and dismissing popover.

---
+++ Example/Pods/Target Support Files/FOPopup/FOPopup-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FOPopup : NSObject
+@end
+@implementation PodsDummy_FOPopup
+@end
---
+++ Example/Pods/Target Support Files/FOPopup/FOPopup-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double FOPopupVersionNumber;
+FOUNDATION_EXPORT const unsigned char FOPopupVersionString[];
+
